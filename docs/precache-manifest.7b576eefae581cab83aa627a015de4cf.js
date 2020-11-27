632fd6df820dc47d046dcfb93a717dd427248079
Andres Käver <akaver@akaver.com>
2020-11-27 09:43:50 +0200

    dist -> docs

---
+++ docs/precache-manifest.7b576eefae581cab83aa627a015de4cf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "553f87f3bd2e25d48b70",
+    "url": "/css/app.3ff004a8.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "8256803a51df77fb4eee93bc6fb1057d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3436ea3ac82482f3e3a0",
+    "url": "/js/about.063687bd.js"
+  },
+  {
+    "revision": "553f87f3bd2e25d48b70",
+    "url": "/js/app.903869f5.js"
+  },
+  {
+    "revision": "898de33fbddfef3c36dd",
+    "url": "/js/chunk-vendors.b562ad2b.js"
+  },
+  {
+    "revision": "0cecec95db209a32781eb20aa8416046",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
